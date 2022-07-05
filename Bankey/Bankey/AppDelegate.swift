dbd3bb23cac7240473316db89081b94a1eaf0983
Your Name <tilek@Tileks-MacBook-Pro.local>
2022-07-05 14:52:15 +0600

    Add login screen

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Tilek on 1/7/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+
+
+}
+
