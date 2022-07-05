bb326ed54d8f27067703e6843b258a48d0e84cf9
Your Name <tilek@Tileks-MacBook-Pro.local>
2022-07-05 18:20:14 +0600

    refactor: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused across multiple onboarding screens

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 class OnboardingViewController: UIViewController{
     
     let stackView = UIStackView()
-    
     let imageView = UIImageView()
-    
     let label = UILabel()
+    
+    let heroImageName: String
+    let titleText: String
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +23,22 @@ class OnboardingViewController: UIViewController{
         layout()
         
     }
-    
-}
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    }
 
 extension OnboardingViewController {
     
     func style() {
+        view.backgroundColor = .systemBackground
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -36,7 +47,7 @@ extension OnboardingViewController {
         //Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         //Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -44,7 +55,7 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that make you feel like you are back in 1989."
+        label.text = titleText
         
         
         
